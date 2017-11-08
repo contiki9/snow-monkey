e9841bb1cb2a38d2044d8d5118c20c78ab8f16fc
travis-ci <inc@2inc.org>
2017-11-08 03:01:09 +0000

    [ci skip] release branch update from travis 6173f881208730af08fd5c37db3f1daaa605f629

--- app/customizer/design.php
+++ app/customizer/design.php
@@ -29,28 +29,54 @@ $control->join( $section );
 /**
  * Layout
  */
-$post_types = get_post_types( [
+$_post_types = get_post_types( [
 	'public' => true,
 ] );
-unset( $post_types['attachment'] );
-foreach ( $post_types as $post_type ) {
-	$post_type_object = get_post_type_object( $post_type );
+unset( $_post_types['attachment'] );
+
+$post_types = [];
+foreach ( $_post_types as $post_type ) {
+	$post_types[ $post_type ] = get_post_type_object( $post_type );
+}
+
+$choices = [
+	'left-sidebar'     => __( 'Left sidebar', 'snow-monkey' ),
+	'right-sidebar'    => __( 'Right sidebar', 'snow-monkey' ),
+	'one-column'       => __( 'One column', 'snow-monkey' ),
+	'one-column-fluid' => __( 'One column (fluid)', 'snow-monkey' ),
+	'one-column-slim'  => __( 'One column (slim)', 'snow-monkey' ),
+];
+
+foreach ( $post_types as $post_type => $post_type_object ) {
+	/**
+	 * singular page
+	 */
 	$customizer->control( 'select', $post_type_object->name . '-layout', [
 		// @codingStandardsIgnoreStart
 		'label'   => sprintf( __( '%1$s layout', 'snow-monkey' ), __( $post_type_object->label ) ),
 		// @codingStandardsIgnoreEnd
 		'default' => 'right-sidebar',
-		'choices' => [
-			'left-sidebar'     => __( 'Left sidebar', 'snow-monkey' ),
-			'right-sidebar'    => __( 'Right sidebar', 'snow-monkey' ),
-			'one-column'       => __( 'One column', 'snow-monkey' ),
-			'one-column-fluid' => __( 'One column (fluid)', 'snow-monkey' ),
-			'one-column-slim'  => __( 'One column (slim)', 'snow-monkey' ),
-		],
+		'choices' => $choices,
 	] );
 
 	$control = $customizer->get_control( $post_type_object->name . '-layout' );
 	$control->join( $section );
+
+	/**
+	 * archive page
+	 */
+	if ( 'post' === $post_type_object->name || ! empty( $post_type_object->has_archive ) ) {
+		$customizer->control( 'select', $post_type_object->name . '-archive-layout', [
+			// @codingStandardsIgnoreStart
+			'label'   => sprintf( __( '%1$s archive layout', 'snow-monkey' ), __( $post_type_object->label ) ),
+			// @codingStandardsIgnoreEnd
+			'default' => 'one-column',
+			'choices' => $choices,
+		] );
+
+		$control = $customizer->get_control( $post_type_object->name . '-archive-layout' );
+		$control->join( $section );
+	}
 }
 
 /**
@@ -127,3 +153,49 @@ $customizer->control( 'checkbox', 'mwt-display-profile-box', [
 $section = $customizer->get_section( 'design' );
 $control = $customizer->get_control( 'mwt-display-profile-box' );
 $control->join( $section );
+
+/**
+ * Custom logo scale
+ */
+$custom_logo = get_custom_logo();
+if ( $custom_logo ) {
+	preg_match( '/height="(\d+?)"/', $custom_logo, $reg );
+	if ( ! isset( $reg[1] ) ) {
+		return;
+	}
+	$height = $reg[1];
+
+	$customizer->control( 'number', 'sm-logo-scale', [
+		'label'       => __( 'Custom logo scale (%) on smartphone', 'snow-monkey' ),
+		'default'     => 33,
+		'input_attrs' => [
+			'min' => 33,
+			'max' => 50,
+		],
+	] );
+
+	$section = $customizer->get_section( 'design' );
+	$control = $customizer->get_control( 'sm-logo-scale' );
+	$control->join( $section );
+}
+
+/**
+ * Display recent posts when using static front page
+ */
+$customizer->control( 'checkbox', 'display-static-front-page-recent-posts', [
+	'label'   => __( 'Display recent posts when using static front page', 'snow-monkey' ),
+	'default' => true,
+	'active_callback' => function() {
+		if ( 'page' === get_option( 'show_on_front' ) && get_option( 'page_on_front' ) ) {
+			return true;
+		}
+		return false;
+	},
+] );
+
+$section = $customizer->get_section( 'design' );
+$control = $customizer->get_control( 'display-static-front-page-recent-posts' );
+$control->join( $section );
+$control->partial( [
+	'selector' => '.home.page-template-default .p-recent-posts__title',
+] );
