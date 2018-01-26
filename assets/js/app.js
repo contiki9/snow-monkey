5c212d0eb6647f0011e6fa0949c2f2505cc017f6
travis-ci <inc@2inc.org>
2018-01-26 08:18:35 +0000

    [ci skip] release branch update from travis d233ff1d9e26c8c781bda7a578e0b679fcaca627

--- assets/js/app.js
+++ assets/js/app.js
@@ -4288,7 +4288,7 @@ var SnowMonkeyPageTopScroll = function SnowMonkeyPageTopScroll() {
           if ('sticky' === $('.l-header').attr('data-l-header-type')) {
             return $('.l-header').outerHeight() + parseInt($('html').css('margin-top'));
           }
-          return $('.l-header__drop-nav').outerHeight() + parseInt($('html').css('margin-top'));
+          return $('.l-header__drop-nav .p-global-nav').outerHeight() + parseInt($('html').css('margin-top'));
         }()
       });
     });
