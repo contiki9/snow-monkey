1ce4f0949ff994b9117df455d0be29dc3fd37544
travis-ci <inc@2inc.org>
2018-02-14 06:32:35 +0000

    [ci skip] release branch update from travis 1d9e1dbb2f3edbd73bcb3c29f6d77a6fff172d09

--- templates/view/content-page.php
+++ templates/view/content-page.php
@@ -8,16 +8,27 @@
 <?php get_template_part( 'template-parts/title-top-widget-area' ); ?>
 
 <article <?php post_class(); ?>>
-	<header class="c-entry__header">
-		<h1 class="c-entry__title"><?php the_title(); ?></h1>
-	</header>
+	<?php if ( 'title-on-page-header' !== get_theme_mod( 'page-eyecatch' ) ) : ?>
+		<header class="c-entry__header">
+			<h1 class="c-entry__title"><?php the_title(); ?></h1>
+		</header>
+	<?php endif; ?>
 
 	<div class="c-entry__content">
+		<?php
+		if ( 'content-top' === get_theme_mod( 'page-eyecatch' ) ) {
+			get_template_part( 'template-parts/eyecatch' );
+		}
+		?>
 		<?php the_content(); ?>
 		<?php get_template_part( 'template-parts/link-pages' ); ?>
 	</div>
 
-	<?php get_template_part( 'template-parts/child-pages' ); ?>
+	<?php
+	if ( get_option( 'mwt-display-child-pages' ) ) {
+		get_template_part( 'template-parts/child-pages' );
+	}
+	?>
 </article>
 
 <?php get_template_part( 'template-parts/contents-bottom-widget-area' ); ?>
--- templates/view/content-post.php
+++ templates/view/content-post.php
@@ -8,27 +8,39 @@
 <?php get_template_part( 'template-parts/title-top-widget-area' ); ?>
 
 <article <?php post_class(); ?>>
-	<header class="c-entry__header">
-		<h1 class="c-entry__title"><?php the_title(); ?></h1>
-		<div class="c-entry__meta">
-			<?php get_template_part( 'template-parts/entry-meta' ); ?>
-		</div>
-
-		<?php
-		wpvc_get_template_part( 'template-parts/share-buttons', [
-			'_position' => 'top',
-		] );
-		?>
-	</header>
+	<?php if ( 'title-on-page-header' !== get_theme_mod( 'post-eyecatch' ) ) : ?>
+		<header class="c-entry__header">
+			<h1 class="c-entry__title"><?php the_title(); ?></h1>
+			<div class="c-entry__meta">
+				<?php get_template_part( 'template-parts/entry-meta' ); ?>
+			</div>
+		</header>
+	<?php endif; ?>
 
 	<div class="c-entry__content">
+		<?php
+		if ( in_array( get_option( 'mwt-share-buttons-display-position' ), [ 'top', 'both' ] ) ) {
+			get_template_part( 'template-parts/share-buttons' );
+		}
+		?>
+
 		<?php
 		wpvc_get_template_part( 'template-parts/google-adsense', [
 			'position' => 'content-top',
 		] );
 		?>
 
-		<?php get_template_part( 'template-parts/contents-outline' ); ?>
+		<?php
+		if ( 'content-top' === get_theme_mod( 'post-eyecatch' ) ) {
+			get_template_part( 'template-parts/eyecatch' );
+		}
+		?>
+
+		<?php
+		if ( get_option( 'mwt-display-contents-outline' ) ) {
+			get_template_part( 'template-parts/contents-outline' );
+		}
+		?>
 
 		<?php the_content(); ?>
 		<?php get_template_part( 'template-parts/link-pages' ); ?>
@@ -36,12 +48,16 @@
 
 	<footer class="c-entry__footer">
 		<?php
-		wpvc_get_template_part( 'template-parts/share-buttons', [
-			'_position' => 'bottom',
-		] );
+		if ( in_array( get_option( 'mwt-share-buttons-display-position' ), [ 'bottom', 'both' ] ) ) {
+			get_template_part( 'template-parts/share-buttons' );
+		}
 		?>
 
-		<?php get_template_part( 'template-parts/profile-box' ); ?>
+		<?php
+		if ( get_option( 'mwt-display-profile-box' ) ) {
+			get_template_part( 'template-parts/profile-box' );
+		}
+		?>
 
 		<?php
 		wpvc_get_template_part( 'template-parts/google-adsense', [
@@ -57,7 +73,11 @@
 
 <?php get_template_part( 'template-parts/contents-bottom-widget-area' ); ?>
 
-<?php get_template_part( 'template-parts/related-posts' ); ?>
+<?php
+if ( get_option( 'mwt-display-related-posts' ) ) {
+	get_template_part( 'template-parts/related-posts' );
+}
+?>
 
 <?php
 if ( comments_open() || pings_open() || get_comments_number() ) {
