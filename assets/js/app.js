48309244dfeb0fe5b27c9234a683cacdc0e798cc
travis-ci <inc@2inc.org>
2018-03-29 00:41:43 +0000

    [ci skip] release branch update from travis ff7187f9db1b3ba05db7484e6249ae335b4dfa75

--- assets/js/app.js
+++ assets/js/app.js
@@ -4377,6 +4377,113 @@ var SnowMonkeyFooterStickyNav = function () {
   return SnowMonkeyFooterStickyNav;
 }();
 
+/**
+ * Name: jquery.contents-outline
+ * Author: Takashi Kitajima (inc2734)
+ * Author URI: https://2inc.org
+ * License: MIT
+ *
+ * @param { headings, moveToBefore1stHeading }
+ */
+
+'use strict';
+
+(function ($$$1) {
+  $$$1.fn.contentsOutline = function (params) {
+    var params = $$$1.extend({
+      headings: $$$1('h2, h3, h4, h5, h6'),
+      moveToBefore1stHeading: true
+    }, params);
+
+    return this.each(function (i, e) {
+      var wrapper = $$$1(e);
+      var co = wrapper.find('.contents-outline');
+
+      var outlines = $$$1('<ol />');
+
+      (function () {
+        params.headings.each(function (i, e) {
+          if (!$$$1(e).attr('id')) {
+            $$$1(e).attr('id', 'co-index-' + i);
+          }
+          outlines = _createTree(outlines, $$$1(e));
+        });
+
+        if (!outlines.html()) {
+          wrapper.remove();
+        }
+
+        if (true === params.moveToBefore1stHeading) {
+          params.headings.first().before(wrapper);
+        }
+
+        wrapper.attr('aria-hidden', 'false');
+        co.append(outlines);
+      })();
+
+      /**
+       * Create tree
+       *
+       * @param   {dom}  parent  The children wrapper element
+       * @param   {dom}  heading  Heading
+       * @param   {Number} hierarchical  Hierarchical
+       * @return  {dom}  The tree
+       */
+      function _createTree(parent, heading) {
+        var hierarchical = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 2;
+
+        var nest = parseInt(heading.prop('tagName').replace('H', '') - hierarchical);
+
+        if (0 > nest) {
+          return parent;
+        }
+
+        if (0 === nest) {
+          parent.append(_createItem(heading));
+        } else {
+          var children = _createSubTree(parent, heading, hierarchical);
+
+          if (1 > parent.children('li').length) {
+            parent.append($$$1('<li />').append(children));
+          } else {
+            parent.children('li:last-child').append(children);
+          }
+        }
+
+        return parent;
+      }
+
+      /**
+       * Create child item
+       *
+       * @param {dom}  heading  Heading
+       * @return {dom}  Child item
+       */
+      function _createItem(heading) {
+        return $$$1('<li />').append($$$1('<a />').text(heading.text()).attr('href', '#' + heading.attr('id')));
+      }
+
+      /**
+       * Create subtree
+       *
+       * @param   {dom}  parent  The children wrapper element
+       * @param   {dom}  heading  Heading
+       * @param   {Number} hierarchical  Hierarchical
+       * @return  {dom}  The tree
+       */
+      function _createSubTree(parent, heading, hierarchical) {
+        var _parent = void 0;
+        if (1 > parent.children('li:last-child').children('ol').length) {
+          _parent = $$$1('<ol />');
+        } else {
+          _parent = parent.children('li:last-child').children('ol');
+        }
+        return _createTree(_parent, heading, hierarchical + 1);
+      }
+    });
+  };
+})(jQuery);
+
 'use strict';
 
 new BasisStickyHeader();
