9d14d723d75158c776d114677cdbd363f10e950c
travis-ci <inc@2inc.org>
2018-04-12 13:53:47 +0000

    [ci skip] release branch update from travis 0b21189a8463ee7fc3c87424855a60c61179d12e

--- app/template-tags/is-output-page-header.php
+++ app/template-tags/is-output-page-header.php
@@ -11,20 +11,22 @@
  * @return boolean
  */
 function snow_monkey_is_output_page_header() {
-	$return = false;
+	$return    = false;
+	$image_url = \Snow_Monkey\app\model\Page_Header_Image_Url::get();
+	$valid_choices = [ 'page-header', 'title-on-page-header' ];
 
 	if ( is_front_page() ) {
 		$return = false;
-	} elseif ( is_page() && 'page-header' === get_theme_mod( 'page-eyecatch' ) ) {
-		$return = true;
-	} elseif ( is_singular( 'post' ) && 'page-header' === get_theme_mod( 'post-eyecatch' ) ) {
-		$return = true;
 	} elseif ( snow_monkey_is_output_page_header_title() ) {
 		$return = true;
-	} elseif ( ! is_singular() ) {
-		$return = true;
-	} elseif ( \Snow_Monkey\app\model\Page_Header_Image_Url::get() ) {
-		$return = true;
+	} elseif ( $image_url ) {
+		if ( is_singular( 'post' ) && in_array( get_theme_mod( 'post-eyecatch' ), $valid_choices ) ) {
+			$return = true;
+		} elseif ( is_page() && in_array( get_theme_mod( 'page-eyecatch' ), $valid_choices ) ) {
+			$return = true;
+		} elseif ( ! is_singular() ) {
+			$return = true;
+		}
 	}
 
 	return apply_filters( 'snow_monkey_is_output_page_header', $return );
